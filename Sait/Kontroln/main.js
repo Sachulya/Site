f590eba39f6fec4c27d42dad635ef5ea89c3c09f
Sachulya <42521024+Sachulya@users.noreply.github.com>
2018-11-14 00:07:59 +0300

    new

---
+++ Sait/Kontroln/main.js
@@ -0,0 +1,33 @@
+﻿document.createElement('details');
+document.createElement('summary');
+$(function() { 
+ 
+  $('details').each(
+    function() {
+ 
+      // configuration
+      $(this).attr('open', true);
+      var action = $(this).find('summary'),
+          expand = $(this).find('section'),         
+          timer  = 950,
+          ex_e   = '▲',
+          ex_c   = '▼';
+ 
+      // set marker & force expands
+      action.append('<i>' + ex_c + '</i>');
+      action.toggle(
+        function() {
+          action.addClass('expand').find('i').text( ex_e );                                                                                                 
+          expand.show(timer);       
+          return false;                            
+        },
+        function() {           
+          action.removeClass('expand').find('i').text( ex_c );
+          expand.hide(timer);
+          return false;
+        }
+      );     
+    }
+  );
+ 
+});
---
+++ Sait/main.js
@@ -0,0 +1,18 @@
+﻿//************меню***************
+function list(index, itext) { 
+  var eNavbar = Ext.get("navbar"); 
+  var cLists = eNavbar.select("UL"); 
+      cLists.setDisplayed(false); 
+      eNavbar.select("LI").removeClass("selected"); 
+  var sSelector = " > li:nth(" + index + ")"; 
+  var eItem = eNavbar.child(sSelector); 
+  var eList = eItem.child("UL"); 
+  if (eList) { 
+    eList.setDisplayed(true); 
+    if (itext) { 
+      sSelector = "LI:has(:contains(" + itext + "))"; 
+      eItem.child(sSelector).addClass("selected"); 
+    } 
+  } else 
+    eItem.addClass("selected"); 
+}
